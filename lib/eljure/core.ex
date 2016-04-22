32726fa0a8b859a4b600d64bab72833eeb068abe
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2016-04-22 07:01:51 +0200

    Added symbol evaluation and 'def'

--- lib/eljure/core.ex
+++ lib/eljure/core.ex
@@ -1,57 +1,94 @@
 defmodule Eljure.Core do
+  import Kernel, except: [apply: 2]
 
   def main do
     IO.puts "Starting Eljure REPL..."
-    loop
+    loop %{"+" => {:function, &({:integer, elem(&1,1) + elem(&2,1)})}}
   end
 
-  defp loop do
-    case read do
-      "" -> loop
+  defp loop env do
+    case String.strip IO.gets "eljure> " do
+      "" -> loop env
 
       "quit" ->; # quit loop
 
       data ->
-        print eval data
-        loop
+        try do
+          {result, newEnv} = eval read(data), env
+          print result
+          loop newEnv
+        rescue 
+          ex in RuntimeError -> 
+            IO.puts ex.message
+            loop env
+        end
     end
   end
 
   # Reads input from stdio, parses and returns ast
-  defp read do
-    "eljure> "
-    |> IO.gets
-    |> String.strip
-    |> Eljure.Reader.read
+  def read code do
+    Eljure.Reader.read code
   end
 
   # Evaluates the AST
-  defp eval ast do
-    # TODO: evaluate the AST
-    ast
+  def eval({:symbol, s}, env) do
+    case Map.fetch(env, s) do
+      {:ok, value} -> {value, env}
+      :error -> raise "Undefined symbol: \"#{s}\""
+    end
   end
 
+  def eval({:list, [{:symbol, "def"}, {:symbol, s}, value]}, env) do
+    {evaledValue, _} = eval value, env
+    {nil, Map.put(env, s, evaledValue)}
+  end
+
+  def eval {:list, l} = ast, env do
+    {f, _} = eval(Enum.at(l, 0), env)
+    args = List.delete_at(l, 0) 
+            |> Enum.map(&(eval(&1, env)))
+            |> Enum.map(&(elem(&1, 0)))
+    {apply(f, args), env}
+  end
+
+  def eval ast, env do
+    {ast, env}
+  end
+
+  def apply {:function, f}, args do
+    Kernel.apply(f, args)
+  end
+
+  def apply head, args do
+    print(head)
+  end
 
   # Prints the resulting value
-  defp print result do
+  def print result do
     IO.puts as_string result
   end
 
-  defp as_string({:symbol, s}), do: to_string(s)
-  defp as_string({:integer, i}), do: to_string(i)
-  defp as_string({:string, s}), do: "\"#{s}\""
-  defp as_string({:keyword, k}), do: ":#{k}"
-  defp as_string({:list, list}) do
+  def as_string(nil), do: "nil"
+  def as_string(true), do: "true"
+  def as_string(false), do: "false"
+  def as_string({:symbol, s}), do: to_string(s)
+  def as_string({:integer, i}), do: to_string(i)
+  def as_string({:string, s}), do: "\"#{s}\""
+  def as_string({:keyword, k}), do: ":#{k}"
+  def as_string({:list, list}) do
     "(#{list |> Enum.map(&as_string/1)
              |> Enum.join(" ")})"
   end
-  defp as_string({:vector, vector}) do
+  def as_string({:vector, vector}) do
     "[#{vector |> Enum.map(&as_string/1)
                |> Enum.join(" ")}]"
   end
-  defp as_string({:map, map}) do
+  def as_string({:map, map}) do
     "{#{map |> Enum.flat_map(fn {k, v} -> [as_string(k), as_string(v)] end)
         |> Enum.join(" ")}}"
+  end
+  def as_string x do
+    to_string(x) <> " !!not atom!!"
   end
 
 end
--- test/eljure/core_test.exs
+++ test/eljure/core_test.exs
@@ -1,7 +1,59 @@
 defmodule EljureTest.Core do
   use ExUnit.Case
   doctest Eljure.Core
+  import Eljure.Core
 
-  test "" do
+  test "should eval symbol to it's value" do
+    env = %{"number" => {:integer, 42}}
+    assert {{:integer, 42}, env} == eval {:symbol, "number"}, env
+  end
+
+  test "should raise error when symbol is not found" do
+    env = %{}
+    assert_raise RuntimeError, "Undefined symbol: \"sym\"", fn ->
+      eval({:symbol, "sym"}, env)
+    end
+  end
+
+  test "should evaluate atoms to themselves" do
+    env = %{}
+    assert {{:integer, 42}, env}   == eval {:integer, 42}, env
+    assert {{:string, "s"}, env}   == eval {:string, "s"}, env
+    assert {{:keyword, "kw"}, env} == eval {:keyword, "kw"}, env
+    assert {{:vector, [1,2]}, env} == eval {:vector, [1,2]}, env
+    assert {{:map, %{a: 2}}, env}  == eval {:map, %{a: 2}}, env
+  end
+
+  test "should eval lists as functions" do
+    # given
+    env = %{
+      "+" => {:function, &({:integer, elem(&1, 1) + elem(&2, 1)})},
+      "a" => {:integer, 1},
+      "b" => {:integer, 2}
+    }
+    expr = {:list, [{:symbol, "+"}, {:symbol, "a"}, {:symbol, "b"}]}
+
+    # then
+   assert {{:integer, 3}, env} == eval expr, env
+  end
+
+  test "'def' should define variables" do
+    env = %{}
+    expr = {:list, [{:symbol, "def"}, {:symbol, "sym"}, {:integer, 5}]}
+    assert {nil, %{"sym" => {:integer, 5}}} == eval expr, env
+  end
+
+  test "'def' should eval value to be set" do
+    # given
+    env = %{"+" => {:function, &({:integer, elem(&1, 1) + elem(&2, 1)})}}
+    add_expr = {:list, [{:symbol, "+"}, {:integer, 1}, {:integer, 2}]}
+    expr = {:list, [{:symbol, "def"}, {:symbol, "sym"}, add_expr]}
+
+    # when
+    {result, updatedEnv} = eval expr, env
+
+    #then
+    assert result == nil
+    assert updatedEnv == Map.put(env, "sym", {:integer, 3})
   end
 end
